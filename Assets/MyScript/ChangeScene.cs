b391b851e030d9a937e7a7a4273c59f262ef869e
wwekiet <kietecw@gmail.com>
2015-03-29 19:41:13 +0700

    Starting Project

---
+++ Assets/MyScript/ChangeScene.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	public void ToHostScene()
+	{
+		Application.LoadLevel("HostScene");
+	}
+
+	public void ToMenuScene()
+	{
+		Application.LoadLevel("Menu");
+	}
+	public void ToJoinScene()
+	{
+		Application.LoadLevel("JoinScene");
+	}
+	public void ToInGameScene()
+	{
+		Application.LoadLevel("InGameScene");
+	}
+}
---
+++ Assets/MyScript/Music.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour {
+
+	Object[] myMusic; // declare this as Object array
+	AudioSource audio;
+	void Awake () {
+		audio = GetComponent<AudioSource> ();
+		myMusic = Resources.LoadAll("Audio",typeof(AudioClip));
+		playRandomMusic ();
+	}
+	
+	void Start (){
+		audio.Play(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!audio.isPlaying)
+			playRandomMusic();
+	}
+	
+	void playRandomMusic() {
+		audio.clip = myMusic[Random.Range(0,myMusic.Length)] as AudioClip;
+		audio.Play();
+	}
+}
